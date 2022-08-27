9c525222e7ade9b63bc52c6ceace44a647139a0d
tedd-E <62875128+tedd-E@users.noreply.github.com>
2022-08-27 00:43:31 -0700

    Initial commit

---
+++ summary.py
@@ -0,0 +1,21 @@
+import pysam
+import gzip
+import shutil
+import os
+
+
+def summary(self):
+    directory = "/test_data/"
+
+    artifact_info = "--BAM SUMMARY--"
+    for bamfile in os.listdir(directory):
+        if bamfile.endswith(".gz"):
+            bamfilepath = os.path.abspath(bamfile)
+            with gzip.open(bamfilepath, 'rb') as f_in, open(bamfilepath.rsplit(".", 1)[0], 'wb') as f_out:
+                shutil.copyfileobj(f_in, f_out)
+            bamfile = bamfile[-3:]
+
+        artifact_info += '\n' + str(pysam.flagstat(bamfile))
+
+    print(artifact_info)
+    return True

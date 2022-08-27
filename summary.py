5102edcc0e756ef895d2d8552f20f754faa4cf66
tedd-E <62875128+tedd-E@users.noreply.github.com>
2022-08-27 01:15:36 -0700

    change path

--- summary.py
+++ summary.py
@@ -10,12 +10,12 @@ def summary():
     artifact_info = "--BAM SUMMARY--"
     for bamfile in os.listdir(directory):
         if bamfile.endswith(".gz"):
-            bamfilepath = os.path.abspath(directory+bamfile)
+            bamfilepath = os.path.abspath(bamfile)
             with gzip.open(bamfilepath, 'rb') as f_in, open(bamfilepath.rsplit(".", 1)[0], 'wb') as f_out:
                 shutil.copyfileobj(f_in, f_out)
             bamfile = bamfile[-3:]
 
-        artifact_info += '\n' + str(pysam.flagstat(os.path.abspath(bamfile)))
+        artifact_info += '\n' + str(pysam.flagstat(directory+bamfile))
 
     print(artifact_info)
     return True

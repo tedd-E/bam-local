1403bb63037c0d7c60a6031820e3692a47c73374
tedd-E <62875128+tedd-E@users.noreply.github.com>
2022-08-28 20:21:30 -0700

    clean comments

--- validate.py
+++ validate.py
@@ -6,19 +6,6 @@ import os
 def validate():
     directory = "test_data/"
 
-    # unzip and generate .bai
-    # for bamfile in os.listdir(directory):
-    #     if bamfile.endswith(".gz"):
-    #         bamfilepath = os.path.abspath(bamfile)
-    #         with gzip.open(bamfilepath, 'rb') as f_in, open(bamfilepath.rsplit(".",1)[0], 'wb') as f_out:
-    #                 shutil.copyfileobj(f_in, f_out)
-    #         bamfile = bamfile[-3:]
-    #     if bamfile.endswith(".bam"):
-    #         try:
-    #             pysam.index(bamfile)
-    #         except Exception:
-    #             print("Unable to generate bai file for bam file %s" % bamfile)
-
     for bamfile in os.listdir(directory):
         if bamfile.endswith(".gz"):
             bamfilepath = os.path.abspath(directory+bamfile)
